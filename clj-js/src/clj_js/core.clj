55f86c464b79ace470f630ca08f95f50f9cf343f
Johannes Lötzsch <github@johannesloetzsch.de>
2018-05-05 02:18:46 +0200

    some minimal java->clojure interop

--- clj-js/src/clj_js/core.clj
+++ clj-js/src/clj_js/core.clj
@@ -10,7 +10,10 @@
         parse (.eval context "js" "JSON.parse")
         stringify (.eval context "js" "JSON.stringify")
         f (.eval context "js" "function (s) {return s + ' :)'}")
-        result (->> (.execute parse (into-array ["{\"a\": 42}"]))
-                    (#(.execute stringify (into-array Object [%])))
-                    (#(.execute f (into-array Object [%]))))]
-       (println result)))
+        json (.execute parse (into-array ["{\"a\": 23, \"b\": 42}"]))
+        result (->> json
+                    #_(#(.execute stringify (into-array Object [%])))
+                    #_(#(.execute f (into-array Object [%]))))]
+       (println result)
+       (println (.getMember json "a"))
+       (println (into {} (.as json java.util.Map)))))
